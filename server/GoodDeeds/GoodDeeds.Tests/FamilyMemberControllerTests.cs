076eeaba843f2b7fdcafbc9c4f432f1eca215a3a
Kiko Carisse <kiko.carisse@borrowell.com>
2022-07-11 20:11:43 -0600

    Setting up unit tests

---
+++ server/GoodDeeds/GoodDeeds.Tests/FamilyMemberControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using GoodDeeds.API.Controllers;
+using GoodDeeds.Core.Dtos;
+using GoodDeeds.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace GoodDeeds.Tests;
+
+public class FamilyMemberControllerTests
+{
+    private readonly FamilyMemberController _sut;
+    private readonly Mock<IFamilyMemberService> _familyMemberServiceMock;
+
+    public FamilyMemberControllerTests()
+    {
+        var loggerMock = new Mock<ILogger<FamilyMemberController>>();
+
+        _familyMemberServiceMock = new Mock<IFamilyMemberService>();
+        _sut = new FamilyMemberController(_familyMemberServiceMock.Object, loggerMock.Object);
+    }
+    
+    [Fact]
+    public void FamilyMemberController_Should_GetCreated()
+    {
+        _sut.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task RetrieveFamilyMemberByIdAsync_Should_Be200Ok_When_RetrieveFamilyMemberByIdAsyncReturnsFamilyDto()
+    {
+        var fixture = new Fixture();
+        var familyMemberDto = fixture.Create<FamilyMemberDto>();
+        
+        _familyMemberServiceMock.Setup(item => item.RetrieveFamilyMemberByIdAsync(It.IsAny<string>())).ReturnsAsync(familyMemberDto);
+
+        var result = await _sut.RetrieveFamilyMemberByIdAsync(It.IsAny<string>());
+        
+        var objResult = result as ObjectResult;
+        objResult.Should().NotBeNull();
+        objResult?.StatusCode.Should().Be(200);
+    }
+}
---
+++ server/GoodDeeds/GoodDeeds.Tests/TestExtensions/LoggerTestExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Gemstone.Loan.API.Tests.TestExtensions;
+
+public static class LoggerTestExtensions
+{
+    public static void VerifyLogging<T>(this Mock<ILogger<T>> logger, LogLevel logLevel, Times times)
+    {
+        logger.Verify(
+            x => x.Log(
+                It.Is<LogLevel>(l => l == logLevel),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => true),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), times);
+    }
+    
+    public static void VerifyLogging<T>(this Mock<ILogger<T>> logger, LogLevel logLevel, string message, Times times)
+    {
+        logger.Verify(
+            x => x.Log(
+                It.Is<LogLevel>(l => l == logLevel),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => string.Equals(message, o.ToString(), StringComparison.InvariantCultureIgnoreCase)),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), times);
+    }
+}
